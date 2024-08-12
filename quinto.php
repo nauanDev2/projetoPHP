962d575928d1b69f052ee67f9feb4ce3c2faf6b6
nauanDev2 <nauan.dev2@gmail.com>
2024-08-12 20:22:10 -0300

    quinto exercício

---
+++ quinto.php
@@ -0,0 +1,17 @@
+<?php
+
+   print "Usando print para exibir na tela <br />";
+
+   $data = "20 de agosto de 2020";
+   $salario = 850.00;
+   $cargo = "Estagiário";
+
+   echo "Arquivo criado em $data <br />";
+
+   printf("Salário mínimo: R$%.2f <br />", $salario);
+
+   $texto = sprintf("%s recebe R$%.2f por mês", $cargo, $salario*2);
+
+   echo $texto;
+
+?>
